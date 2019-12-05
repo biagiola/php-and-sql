db783ef5625330be27e3d0242e2a8504f4a23b4d
David <41311088+biagiola@users.noreply.github.com>
2019-12-05 07:13:16 -0300

    explode function

--- index.php
+++ index.php
@@ -40,7 +40,11 @@
                 <div class="card z-depth-0">
                     <div class="card-content center">
                         <h6> <?php echo htmlspecialchars( $pizza['title'] ); ?> </h6>
-                        <div><?php echo htmlspecialchars( $pizza['ingredients'] );  ?></div>
+                        <ul>
+                            <?php foreach( explode(',', $pizza['ingredients']) as $ingredient ) { ?>
+                                <li> <?php echo htmlspecialchars( $ingredient ); ?> </li>
+                            <?php } ?>
+                        </ul>
                     </div>
                     <div class="card-action right-align">
                         <a href="#" class="bran-text">more info</a>
