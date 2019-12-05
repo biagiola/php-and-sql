2e4628d7fdf97d1f142dee9790dca0f8d2e45f11
David <41311088+biagiola@users.noreply.github.com>
2019-12-05 06:24:27 -0300

    getting data from db

--- index.php
+++ index.php
@@ -8,8 +8,20 @@
     if( !$conn ) {
         echo 'Connection error: ' . mysqli_connect_error();
     } else {
-        
+
     }
+
+    // write query for all pizzas
+    $sql = 'SELECT title, ingredients, id FROM pizzas';
+
+    // make query and get result
+    $result = mysqli_query($conn, $sql);
+
+    // fetch the resulting rows as an array
+    $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC); // MYSQLI_ASSOC means the way that we want the return (as an array)
+
+    print_r($pizzas);
+
 
 ?>
 <!DOCTYPE html>
